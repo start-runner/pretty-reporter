695aeeaa96a9d1db37df28dafcacefe754e680eb
deepsweet <kir@soulshine.in>
2016-12-29 15:44:04 +0100

    :wrench: switch to `eslint-config-tough`

--- test/lib/index.js
+++ test/lib/index.js
@@ -2,18 +2,18 @@ import test from 'tape';
 
 import reporter from '../../lib/';
 
-test('basic', t => {
-    t.equal(
-        typeof reporter,
-        'function',
-        '1st function'
-    );
+test('basic', (t) => {
+  t.equal(
+    typeof reporter,
+    'function',
+    '1st function'
+  );
 
-    t.equal(
-        typeof reporter(),
-        'function',
-        '2nd function'
-    );
+  t.equal(
+    typeof reporter(),
+    'function',
+    '2nd function'
+  );
 
-    t.end();
+  t.end();
 });
